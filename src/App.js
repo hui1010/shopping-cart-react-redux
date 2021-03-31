9e1966548dac3c8f9fb2168d221d7d46081c265c
Huiyi Skårner <63238749+hui1010@users.noreply.github.com>
2021-03-31 20:05:08 +0200

    created product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import {useState} from 'react'
+
+import data from './data.json'
+import Products from './components/Products'
+
 // feature-1
 function App() {
+  const [products, setProducts] = useState(data.products)
+  const [size, setSize] = useState("")
+  const [sort, setSort] = useState("")
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main" >
+            <Products products={products}/>
+          </div>
+          <div className="sidebar">
+            Cart Items
+          </div>
+        </div>
       </main>
       <footer>
         All right is reserved.
