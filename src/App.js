6a2451b74a4d2a877e6617ed68dcc55ed6d5a77b
Huiyi Skårner <63238749+hui1010@users.noreply.github.com>
2021-03-31 22:00:03 +0200

    Created filter component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import {useState} from 'react'
 
 import data from './data.json'
 import Products from './components/Products'
+import Filter from './components/Filter'
 
 // feature-1
 function App() {
   const [products, setProducts] = useState(data.products)
   const [size, setSize] = useState("")
   const [sort, setSort] = useState("")
+
+  const sortProducts = (event) => {
+    const targetSort = event.target.value
+    setSort(targetSort)
+    setProducts(
+      products.slice().sort((a, b) => 
+        targetSort == "lowest" ? a.price - b.price : targetSort =="highest" ? b.price - a.price : b._id - a._id
+      )
+    )
+  }
+  const filterProducts = (event) => {
+    const targetSize = event.target.value
+    setSize(targetSize)
+    if(targetSize) {
+      setProducts(
+        products.filter(product => product.availableSizes.indexOf(targetSize) >= 0)
+      )
+    } 
+  }
+  
 
   return (
     <div className="grid-container">
@@ -17,6 +38,13 @@ function App() {
       <main>
         <div className="content">
           <div className="main" >
+            <Filter 
+              count={products.length} 
+              size={size} 
+              sort={sort}
+              sortProducts={sortProducts}
+              filterProducts={filterProducts}  
+            />
             <Products products={products}/>
           </div>
           <div className="sidebar">
---
+++ src/components/Filter.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+function Filter({count, size, sort, sortProducts, filterProducts}) {
+
+    
+
+    return (
+        <div className="filter"> 
+            <div className="filter-re">
+                {count} products
+            </div>
+            <div className="filter-sort">
+                Order By {" "}
+                <select value={sort} onChange={sortProducts}>
+                    <option value="">Latest</option>
+                    <option value="lowest">Lowest price</option>
+                    <option value="highest">Highest price</option>
+                </select>
+            </div>
+            <div className="filter-size">
+                Filter Size {" "}
+                <select value={size} onChange={filterProducts}>
+                    <option value="">ALL</option>
+                    <option value="XS">XS</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                    <option value="XXL">XXL</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default Filter
